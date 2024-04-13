645412f277d74b4ca3c0a01d4739e5ced1bac77b
FrancescoTorella <163884362+FrancescoTorella@users.noreply.github.com>
2024-04-13 16:53:40 +0200

    Delete Front-end directory

--- Front-end/src/scripts/modalitaViaggio/constants.js
+++
@@ -1,9 +0,0 @@
-//salva il colore iniziale dei bottoni quadrati
-export const defaultSquarebuttonsColor = 'rgb(245, 245, 245)';
-
-//salva il colore iniziale dei bottoni sottili
-export const defaultThinbuttonsColor = 'white';
-
-//numero di righe e colonne
-export const rows = 32;
-export const cols = 32;
