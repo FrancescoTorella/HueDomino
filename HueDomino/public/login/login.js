6304066b7143e2c96224efe0cde948e28f2e38e6
FrancescoTorella <163884362+FrancescoTorella@users.noreply.github.com>
2024-04-24 15:33:26 +0200

    Add files via upload

---
+++ HueDomino/public/login/login.js
@@ -0,0 +1,27 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // Login riuscito
+        console.log('Login riuscito');
+      } else {
+        // Login fallito
+        console.log('Login fallito');
+      }
+    })
+    .catch((error) => {
+      console.error('Errore:', error);
+    });
+  });
---
+++ HueDomino/public/login/signin/register.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const registerForm = document.getElementById('registerForm');
+
+    registerForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        console.log("submit");
+
+        const formData = new FormData(registerForm);
+        const username = formData.get('username');
+        const password = formData.get('password');
+
+        const response = await fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.status === 201) {
+            // Registrazione riuscita
+            console.log('Registrazione riuscita');
+        } else {
+            // Registrazione fallita
+            console.log('Registrazione fallita');
+        }
+    });
+});
