25c0f47e608252e258ccea05ab7bcb2fee0b40f7
Federico Zaza <164476534+FZaza25@users.noreply.github.com>
2024-04-26 00:30:45 +0200

    Update scripts.js

--- HueDomino/public/viaggio/italia/scripts.js
+++ HueDomino/public/viaggio/italia/scripts.js
@@ -89,4 +89,36 @@ document.addEventListener('DOMContentLoaded', function() {
             dropdown.style.backgroundColor = 'rgba(100, 100, 100, 1)';
         }
     });
+
+ // Funzione per aggiustare la dimensione del right box
+ function adjustRightBoxSize() {
+    const rightBox = document.querySelector('.right-box-container');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    if (width <= 500) {
+        rightBox.style.width = "90vw"
+        rightBox.style.aspectRatio = "6/10";
+    } else if (width > 500 && width <= 800) {
+        rightBox.style.width = "80vw"; 
+    } else if (width >= 800 && width <= 1000) {
+        rightBox.style.width = "75vw";
+    }else if (width > 1000 && width <= 1500) {
+        rightBox.style.width = "50vw";
+    }else if (width > 1500 && width <= 2000) {
+        rightBox.style.width = "40vw"; 
+    }else if (width > 2000 && width <= 2500) {
+        rightBox.style.width = "35vw";
+    }else if (width > 2500 && width <= 3000) {
+        rightBox.style.width = "30vw";
+    } else {
+        rightBox.style.width = "25vw";
+    }
+}
+
+// Ascolta per cambiamenti di dimensione della finestra
+window.addEventListener('resize', adjustRightBoxSize);
+
+// Chiamata iniziale per impostare le dimensioni al caricamento della pagina
+adjustRightBoxSize();
 });
