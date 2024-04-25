1a8d159865455e43857fd4fc478eb2773668f23f
FrancescoTorella <163884362+FrancescoTorella@users.noreply.github.com>
2024-04-25 09:34:22 +0200

    Add files via upload

---
+++ viaggio/italia/scripts/data.js
@@ -0,0 +1,6 @@
+//strutture dati
+// Crea una matrice per memorizzare i bottoni quadrati
+export let matrix = [];
+
+//crea una mappa per memorizzare i bottoni sottili
+export let thinButtonsMap = new Map();
