3d6adf64cf1d54971673f3bc6fc78362efbf0d7b
FZaza25 <federicozaza.03@gmail.com>
2024-04-27 13:01:55 +0200

    aggiunta islanda

--- HueDomino/public/viaggio/islanda/scripts.js
+++ HueDomino/public/viaggio/islanda/scripts.js
@@ -90,31 +90,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Funzione per aggiustare la dimensione del right box
+     // Funzione per aggiustare la dimensione del right box
     function adjustRightBoxSize() {
         const rightBox = document.querySelector('.right-box-container');
         const width = window.innerWidth;
         const height = window.innerHeight;
 
         if (width <= 500) {
-            rightBox.style.width = "90vw"
-            rightBox.style.aspectRatio = "6/10";
+            rightBox.style.top = "20vh"
+            rightBox.style.width = "95vw"
         } else if (width > 500 && width <= 800) {
-            rightBox.style.width = "80vw"; 
+            rightBox.style.width = "90vw"; 
         } else if (width >= 800 && width <= 900) {
-            rightBox.style.width = "75vw";
-        }else if (width >900 && width <= 1000) {
-            rightBox.style.width = "60vw";
+            rightBox.style.width = "90vw";
+        }else if (width >900 && width <= 1000) { 
+            rightBox.style.width = "90vw";
         }else if (width > 1000 && width <= 1500) {
-            rightBox.style.width = "50vw";
+            rightBox.style.width = "65vw";
         }else if (width > 1500 && width <= 2000) {
-            rightBox.style.width = "40vw"; 
+            rightBox.style.width = "55vw"; 
         }else if (width > 2000 && width <= 2500) {
-            rightBox.style.width = "35vw";
+            rightBox.style.width = "45vw";
         }else if (width > 2500 && width <= 3000) {
-            rightBox.style.width = "30vw";
+            rightBox.style.width = "40vw";
         } else {
-            rightBox.style.width = "25vw";
+            rightBox.style.width = "35vw";
         }
     }
 
