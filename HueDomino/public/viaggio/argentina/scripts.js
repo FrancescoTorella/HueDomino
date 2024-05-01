61638c127bcbece4b95f7c6d7028a6f677fb9983
FZaza25 <federicozaza.03@gmail.com>
2024-05-01 13:35:10 +0200

    argentina mappa

--- HueDomino/public/viaggio/argentina/scripts.js
+++ HueDomino/public/viaggio/argentina/scripts.js
@@ -8,24 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
         if (width <= 500) {
             rightBox.style.width = "90vw";
             rightBox.style.top = "20vh";
-            //rightBox.style.aspectRatio = "6/10";
         } else if (width > 500 && width <= 800) {
-            rightBox.style.width = "80vw";
-            rightBox.style.top = "5vh"; 
+            rightBox.style.width = "70vw"; 
+            rightBox.style.top = "15vh";
         } else if (width >= 800 && width <= 900) {
-            rightBox.style.width = "75vw";
+            rightBox.style.width = "60vw";
+            rightBox.style.top = "15vh";
         }else if (width >900 && width <= 1000) {
             rightBox.style.width = "60vw";
+            rightBox.style.top = "15vh";
         }else if (width > 1000 && width <= 1500) {
-            rightBox.style.width = "50vw";
+            rightBox.style.width = "43vw";
+            rightBox.style.top = "12vh";
         }else if (width > 1500 && width <= 2000) {
-            rightBox.style.width = "40vw"; 
+            rightBox.style.width = "33vw"; 
+            rightBox.style.top = "12vh";
         }else if (width > 2000 && width <= 2500) {
-            rightBox.style.width = "35vw";
+            rightBox.style.width = "26vw";
+            rightBox.style.top = "12vh";
         }else if (width > 2500 && width <= 3000) {
-            rightBox.style.width = "30vw";
+            rightBox.style.width = "22vw";
+            rightBox.style.top = "12vh";
         } else {
-            rightBox.style.width = "25vw";
+            rightBox.style.width = "20vw";
+            rightBox.style.top = "12vh";
         }
     }
 
